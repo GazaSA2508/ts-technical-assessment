65e82c2831d2501540da3262e1d30e8f5d89990e
garreth <garreth@digitalcrayon.co.za>
2025-11-19 21:48:30 +0200

    feat: add new method GetPitDepth
    Added a new method to calculate the deepest pit in an array of integers

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -6,4 +6,26 @@ public class DeepestPitAnswer
     {
         throw new NotImplementedException("Not completed yet");
     }
+
+    private static int GetPitDepth(int[] points)
+    {
+        //### Note - Algorithm used to calculate depth
+        // depth = min(A[P] - A[Q], A[R] - A[Q])
+
+        //if (points == null) throw new ArgumentNullException(nameof(points));
+        ArgumentNullException.ThrowIfNull(points);
+
+        int length = points.Length;
+
+        if (length < 3) 
+            return -1;
+
+
+        int maxDepth = -1;
+
+
+
+        return maxDepth;
+
+    }
 }
