c812d014f2ebef1d6aafad5f2c6c6f6e5ee3f30f
garreth-shipco <ghall@shipco.com>
2025-11-19 20:43:39 +0200

    feat: add new method GetMaxWordCount
    Added a new method to get the max word count from a string containing any number of sentences

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,7 +5,39 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
+        //### Calculate longest sentence
+
 
         throw new NotImplementedException("Not completed yet");
     }
+
+
+    private static int GetMaxWordCount(string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return 0;
+
+        int maxCount = 0;
+
+        //### Array of characters used to split sentencess
+        char[] delimiters = { '.', '?', '!' };
+
+        //### Split text into sentences
+        var sentences = text.Split(delimiters);
+
+        foreach (var sentence in sentences) 
+        { 
+            string newSentence = sentence.Trim();
+
+
+
+        }
+
+        return maxCount;
+
+
+    }
+
+
+
 }
