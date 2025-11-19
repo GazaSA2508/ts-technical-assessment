c3631a4c6c685608fe54b12e70d5bd1955d90848
garreth <garreth@digitalcrayon.co.za>
2025-11-19 22:12:05 +0200

    feat: update GetPitDepth method
    Updated GetPitDepth method to include calculations to determine pit depth

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -7,25 +7,60 @@ public class DeepestPitAnswer
         throw new NotImplementedException("Not completed yet");
     }
 
-    private static int GetPitDepth(int[] points)
+    private static int GetPitDepth(int[] A)
     {
         //### Note - Algorithm used to calculate depth
         // depth = min(A[P] - A[Q], A[R] - A[Q])
-
-        //if (points == null) throw new ArgumentNullException(nameof(points));
-        ArgumentNullException.ThrowIfNull(points);
-
-        int length = points.Length;
-
-        if (length < 3) 
+        
+        if (A == null || A.Length < 3)
             return -1;
 
-
+        int length = A.Length;
+        int i = 0;
         int maxDepth = -1;
 
+        while (i < length - 1)
+        {
+            //### Look for strictly decreasing start
+            if (A[i] <= A[i + 1])
+            {
+                i++;
+                continue;
+            }
+
+            int p = i;
+
+            //### Walk downward to Q
+            while (i < length - 1 && A[i] > A[i + 1])
+                i++;
+
+            int q = i;
+
+            //### If no room to rise, break
+            if (q == length - 1)
+                break;
+
+            //### Walk upward to R
+            while (i < length - 1 && A[i] < A[i + 1])
+                i++;
+
+            int r = i;
+
+            //### Valid pit must have P < Q < R
+            if (p < q && q < r)
+            {
+                int leftDrop = A[p] - A[q];
+                int rightRise = A[r] - A[q];
+                int depth = Math.Min(leftDrop, rightRise);
+
+                if (depth > maxDepth)
+                    maxDepth = depth;
+            }
+        }
 
 
         return maxDepth;
 
     }
+
 }
